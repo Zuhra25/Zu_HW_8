1aa6de4365ffd7e833d5094ccce1cc408b1a60d4
ZU <zshaihova@gmail.com>
2023-04-24 21:36:42 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        // task2();
+        //task1();
+        task2();
     }
 
     public static void task1() {
@@ -12,7 +12,32 @@ public class Main {
         one[2] = 3;
         double[] two = {1.57, 7.654, 9.986};
         int[] three = {1, 3, 5};
+    }
 
-
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        double[] two = {1.57, 7.654, 9.986};
+        int[] three = {1, 3, 5};
+        for (int i = 0; i <= 2; i++) {
+            if (i != 2)
+                System.out.print(one[i] + ", ");
+            else
+                System.out.println(one[i]);
+        }
+        for (int i = 0; i <= 2; i++)
+            if (i != 2)
+                System.out.print(two[i] + ",  ");
+            else
+                System.out.println(two[i]);
+        for (int i = 0; i < 3; i++) {
+            if (i != 2)
+                System.out.print(one[i] + ", ");
+            else
+                System.out.println(one[i]);
+        }
     }
 }
