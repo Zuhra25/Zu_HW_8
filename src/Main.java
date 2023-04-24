3e8ac65daa965c6bd89bcfcac2e5140ed03d1f30
ZU <zshaihova@gmail.com>
2023-04-24 21:22:42 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        // task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        double[] two = {1.57, 7.654, 9.986};
+        int[] three = {1, 3, 5};
+
 
     }
 }
